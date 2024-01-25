2d993344040c1294a6afb77f4af57d0777b81b39
Oluwasetemi Ojo <setemiojo@gmail.com>
2024-01-24 20:07:53 -0500

    prototypes and contructors

--- promiseAsync.js
+++ promiseAsync.js
@@ -120,7 +120,7 @@ function Person(name) {
 const setemi = new Person('oluwasetemi')
 console.log(setemi)
 
-// classes
+// classes - syntactic sugar of function constructor
 class Person2 {
   constructor(name) {
     this.name = name
@@ -129,3 +129,22 @@ class Person2 {
 
 const setemi2 = new Person2('oluwasetemi')
 console.log(setemi2)
+
+let az = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// new Array(1, 2, 3, 4, 5, 6, 7, 8, 9)
+console.log(typeof az)
+let str1 = 'oluwasetemi' // literal
+// new String('oluwasetemi')
+console.log(typeof str1)
+str1.repeat()
+
+let num = 20
+// new Number(20)
+num.toFixed(2)
+let bool = true
+// new Boolean(true)
+let obj2 = {
+  name: 'oluwasetemi',
+  age: 20,
+}
+// new Object({name: 'oluwasetemi', age: 20})
---
+++ protypes.js
@@ -0,0 +1,92 @@
+// prototypes manipulation
+String.prototype.alt = 'another way'
+String.prototype.altSchool = function() {
+  return 'altSchool'
+}
+
+let name = 'oluwasetemi' // literal	1 - use constructor to construct a new variable, 2 - converts the constructed object to a primitive data type
+// attach properties and methods to the new primitive data type
+console.log(typeof name);
+
+let ab = 'good'
+console.log(ab.alt)
+console.log(ab.altSchool())
+console.log(name.capitalize())
+
+String.prototype.capitalize = function() {
+  const first = this.charAt(0).toUpperCase()
+  const rest = this.slice(1)
+  return first + rest
+}
+
+
+console.log(name.altSchool())
+let a = 2;
+// a.altSchool()
+// contructors
+
+// let button = new HTMLButtonElement()
+// console.log(button)
+// String, Number, Boolean, Array, Object, Function, Promise, RegExp, Date, Error etc
+
+let now = new Date()
+let before = new Date('2020-01-01')
+let before2 = new Date(2020, 0, 1, 23, 59, 59, 999)
+console.log(before2)
+let err = new Error('this is an error')
+console.log(err.stack)
+console.log(now.getDate())
+
+console.log(Math.PI)
+
+// oven that makes bread
+function Ojo(name) {
+  // if (!new.target) {
+  //   return new Ojo(name)
+  // }
+  // let this = {}
+  // property
+  this.name = name
+  // method
+  this.giveBirth = function(name) {
+    console.log('give birth')
+    console.log(name)
+  }
+
+  // return this
+}
+
+let bread1 = new Ojo('oluwasetemi')
+console.log(bread1)
+
+console.log(bread1 instanceof Ojo)
+
+let whiteBread = new Ojo('white')
+let agegeBread = new Ojo('agege')
+
+// bread
+const ojo = {
+  name: 'oluwasetemi',
+  giveBirth: function(name) {
+    console.log('give birth')
+    console.log(name)
+  }
+}
+
+// copying shallow copy, deep copy JSON.parse(JSON.stringify(obj)), Object.assign({}, obj), spread operator, lodash, StructuredClone()
+
+
+const setemi = new Ojo('oluwasetemi')
+
+function add(a, b) {
+  return a + b
+}
+
+function addTrouble(a) {
+  return function(b) {
+    return a + b
+  }
+}
+
+console.log(add(1, 2))
+console.log(addTrouble(10)(20))

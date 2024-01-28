3dcd22d710efcc1b95aa8f3f73e7adc4778524f6
Afeez Adesina <afeezadesinaa@gmail.com>
2024-01-28 14:02:54 +0100

    my test code

---
+++ mine.js
@@ -0,0 +1,11 @@
+str = 'adesina afeez'
+
+// const toUpperCase = str => str.toUpperCase()
+
+const name = function toUpperCase(str){
+        return str.toUpperCase()
+    }
+
+console.log(name(str))
+
+toLowerCase = str => str.toLowerCase();
